bf7f9ad58468ca594f6cb53707e738e8ea60e019
wyw <wen_005@qq.com>
2017-02-20 00:50:21 +0800

    更新代码
    更新代码

---
+++ sys-stock-parent/stock-model-api/src/main/java/com/stock/security/user/model/User.java
@@ -0,0 +1,26 @@
+package com.stock.security.user.model;
+
+/**
+ * 用户
+ * @author wyw
+ * @date:2017年2月20日 上午12:39:46
+ */
+public class User {
+	private Long id;
+	private String userName;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+
+}
